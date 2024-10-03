f8fafb48dd9903b9a805285ab4bcd1199e4d3f83
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2024-10-03 19:17:27 +0530

    fixes

--- twitter-client/next.config.mjs
+++ twitter-client/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['https://marketplace.canva.com/EAFXS8-cvyQ/1/0/1600w/canva-brown-and-light-brown%2C-circle-framed-instagram-profile-picture-2PE9qJLmPac.jpg'],
-    },
+    // images: {
+    //     domains: ['https://marketplace.canva.com/EAFXS8-cvyQ/1/0/1600w/canva-brown-and-light-brown%2C-circle-framed-instagram-profile-picture-2PE9qJLmPac.jpg'],
+    // },
 };
 
 export default nextConfig;
